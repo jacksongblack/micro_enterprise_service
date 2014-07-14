a15375329af77114afad9bef75167f27d82b9705
jacksongblack <songyuchao@163.com>
2014-07-14 11:22:32 +0800

    Modifications due paperclip validation error according to the error

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -4,6 +4,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
                     :styles => { :content => '800>', :thumb => '118x100#' }
 
+  validates_attachment_content_type :data, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/products/missing.png"
 
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   validates :name, presence: true, :length => { :in => 1..15}
   validates :price,  presence: true, :length => { :in => 1..8}
   validates :description, presence: true
