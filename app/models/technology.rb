fce9913074d40e5ac367ce1e44c19f0fa688a5e9
jacksongblack <jacksongemail@163.com>
2014-07-14 23:20:28 +0800

    修改由于验证找成的科技服务超市图片上传bug

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,5 +1,6 @@
 class Technology < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/technology/missing.png"
+  validates_attachment_content_type :avatar,:content_type =>%w{image/jpeg image/jpg image/png}
 
   validates :name, presence: true, :length => { :in => 1..10}
   validates :address, presence: true
